1c1d65dd859e42345317516355993cdeb11c0c4a
Sarah Betack <msbetack@gmail.com>
2017-06-13 11:48:38 -0500

    add exercise

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+  // The code below allows the computer to pick a random choice between: rock, paper, or scissors.
+  var options = ["rock", "paper", "scissors"];
+  var computerChoice = options[Math.floor(Math.random() * options.length)];
+  console.log("The computer chooses: " + computerChoice);
+  // Your code goes below
+  
+
+
+
+
+
+
+
+
+})
