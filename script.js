2b5071f36805323bc6bfbbc07cf42e08459caa8d
Sarah Betack <msbetack@gmail.com>
2017-06-13 12:47:36 -0500

    fix script

--- script.js
+++ script.js
@@ -3,9 +3,15 @@ document.addEventListener('DOMContentLoaded', function() {
   // The code below allows the computer to pick a random choice between: rock, paper, or scissors.
   var options = ["rock", "paper", "scissors"];
   var computerChoice = options[Math.floor(Math.random() * options.length)];
+  // Your prompt() goes below
+
+
+
+
+
+
   console.log("The computer chooses: " + computerChoice);
-  // Your code goes below
-  
+  // Your declareWinner function goes below
 
 
 
