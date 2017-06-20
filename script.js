419cd67aa1a2bf5b17aaf2378af9e879ee8d9b19
Hack Reactor Student <student@hackreactor.com>
2017-06-20 14:31:19 -0500

    Started rock-paper-scissors javascript lesson, started functions and if else statements

--- script.js
+++ script.js
@@ -4,19 +4,29 @@ document.addEventListener('DOMContentLoaded', function() {
   var options = ["rock", "paper", "scissors"];
   var computerChoice = options[Math.floor(Math.random() * options.length)];
   // Your prompt() goes below
+  var userChoice = prompt("Choose one: rock, paper, or scissors")
 
 
 
 
-
-
+  console.log("The user chooses: " + userChoice)
   console.log("The computer chooses: " + computerChoice);
   // Your declareWinner function goes below
+  function declareWinner(userChoice, computerChoice){
+    if (userChoice == "rock"){
+      if (computerChoice == "scissors"){
+        return "You won!"
+      } else if (computerChoice == "paper"){
+        return "The computer won!"
+      } else {
+        return "No one won!"
+      }
+    } else if (userChoice == "paper"){
 
+    } else if (userChoice == "scissors"){
 
-
-
-
+    }  
+  }
 
 
 
