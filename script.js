9d79bdb0f047d03d75adc9fca73fcc1236ad8214
Hack Reactor Student <student@hackreactor.com>
2017-06-21 11:59:17 -0500

    worked on rock paper scisors game

--- script.js
+++ script.js
@@ -4,7 +4,41 @@ document.addEventListener('DOMContentLoaded', function() {
   var options = ["rock", "paper", "scissors"];
   var computerChoice = options[Math.floor(Math.random() * options.length)];
   // Your prompt() goes below
+var userChoice = prompt("choose rock, paper, or scissors")
+console.log("the user chooses: " + userChoice)
 
+function declareWinner (computerChoice, userChoice){
+  if (computerChoice == "rock" && userChoice == "scissors" ){
+    alert("The Computer Won!")
+  }
+  else if (computerChoice == "rock" && userChoice == "paper"){
+    alert("The User Won!")
+  }
+  else if (computerChoice == "rock" && userChoice == "rock"){
+    alert ("It's A Tie!")
+  }
+  else if (computerChoice == "paper" && userChoice == "scissors"){
+    alert ("The User Won!")
+  }
+   else if (computerChoice == "paper" && userChoice == "paper"){
+    alert("Its A Tie!")
+  }
+  else if (computerChoice == "paper" && userChoice == "rock"){
+    alert("The Computer Won!")
+  }
+ else if (computerChoice == "scissors" && userChoice == "scissors"){
+    alert("Its A Tie!")
+  }
+ else if (computerChoice == "scissors" && userChoice == "paper"){
+    alert("The Computer Won!")
+  }
+ else if (computerChoice == "scissors" && userChoice == "rock"){
+    alert("The User Won!")
+  }
+    
+}
+
+declareWinner(computerChoice, userChoice)
 
 
 
