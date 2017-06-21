a7ee0ce356fa521342001b22b9d6737691659679
Hack Reactor Student <student@hackreactor.com>
2017-06-21 11:59:03 -0500

    rps

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
 document.addEventListener('DOMContentLoaded', function() {
 
   // The code below allows the computer to pick a random choice between: rock, paper, or scissors.
-  var options = ["rock", "paper", "scissors"];
-  var computerChoice = options[Math.floor(Math.random() * options.length)];
-  // Your prompt() goes below
-
-
-
-
-
-
-  console.log("The computer chooses: " + computerChoice);
-  // Your declareWinner function goes below
+  var pick = Math.floor(Math.random() * 3) + 1  
+  var userpick = prompt("rock paper or scissors")
+  if(pick == 3){
+    var comp = "rock" 
+  }else if(pick == 2){
+    var comp = "scissors"
+  }else{
+    var comp = "paper"
+  }
+  if (userpick == "rock" && comp == "scissors"){
+    alert("you win comp picked " + comp + " you picked " + userpick)
+  }else if (userpick == "rock" && comp == "paper"){
+    alert("you lose comp picked " + comp + " you picked " + userpick)
+  }else if (userpick == "scissors" && comp == "paper"){
+    alert("you win comp picked " + comp + " you picked " + userpick)
+  }else if(userpick == "scissors" && comp == "rock"){
+    alert("you lose comp picked " + comp + " you picked " + userpick)
+  }else if (userpick == "paper" && comp == "scissors"){
+    alert("you lose comp picked " + comp + " you picked " + userpick)
+  }else if(userpick == "paper" && comp == "rock"){
+    alert("you win  comp picked " + comp + " you picked " + userpick)
+  }else{
+    alert("tie. comp picked " + comp + " you picked " + userpick)
+  }
 
 
 
