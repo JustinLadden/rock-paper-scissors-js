76d5a2de3964b2bc6680aa34b65ce38b30260ef4
Hack Reactor Student <student@hackreactor.com>
2017-06-21 11:58:17 -0500

    Rock Paper Scissors js

--- script.js
+++ script.js
@@ -4,16 +4,53 @@ document.addEventListener('DOMContentLoaded', function() {
   var options = ["rock", "paper", "scissors"];
   var computerChoice = options[Math.floor(Math.random() * options.length)];
   // Your prompt() goes below
+var userChoice = prompt("rock paper or scissors")
 
-
-
+alert(declareWinner(userChoice, computerChoice));
 
 
 
   console.log("The computer chooses: " + computerChoice);
   // Your declareWinner function goes below
 
+function declareWinner() {
 
+if (userChoice =="rock" && computerChoice =='rock') {
+
+return "tie"
+
+} else if (userChoice =="paper" && computerChoice =='paper') {
+
+return "tie"
+
+} else if (userChoice =="scissors" && computerChoice =='scissors') {
+
+return "tie"
+
+} else if (userChoice =="rock" && computerChoice =='paper') {
+return "You lost..."
+
+} else if (userChoice =="rock" && computerChoice =='scissors') {
+return "You won!"
+
+} else if (userChoice =="paper" && computerChoice =='scissors') {
+return "You lost..."
+
+} else if (userChoice =="paper" && computerChoice =='rock') {
+return "You won!"
+
+} else if (userChoice =="scissors" && computerChoice =='paper') {
+return "You won!"
+
+} else if (userChoice =="scissors" && computerChoice =='rock') {
+return "You lost..."
+
+} else {
+
+return "you messed up the game"
+}
+
+}
 
 
 
