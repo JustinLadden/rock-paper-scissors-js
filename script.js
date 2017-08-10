87ed19695eb4b8b087d61ad12aff4743ae043efa
Hack Reactor Student <student@hackreactor.com>
2017-08-10 12:28:54 -0500

    Whatever you want

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-
   // The code below allows the computer to pick a random choice between: rock, paper, or scissors.
   var options = ["rock", "paper", "scissors"];
   var computerChoice = options[Math.floor(Math.random() * options.length)];
@@ -13,7 +12,11 @@ console.log("The user chooses: " + userChoice);
 
   console.log("The computer chooses: " + computerChoice);
   // Your declareWinner function goes below
-function(declareWinner)
+function declareWinner(userChoice,computerChoice){
+  if ("userChoice is paper")
+   statement1
+}
+
 
 
 
