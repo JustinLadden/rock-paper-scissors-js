d051fa7652f6f36e635a3ab1581e808c1ba6de76
Hack Reactor Student <student@hackreactor.com>
2017-08-10 15:34:14 -0500

    Rock,Paper,Scissors

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
+
   // The code below allows the computer to pick a random choice between: rock, paper, or scissors.
   var options = ["rock", "paper", "scissors"];
   var computerChoice = options[Math.floor(Math.random() * options.length)];
   // Your prompt() goes below
 var userChoice = prompt("rock,paper,scissors")
 console.log("The user chooses: " + userChoice);
-
+if (userChoice === "rock" && computerChoice === "scissors") {
+   alert('you win')
+ } else if (userChoice === "paper" && computerChoice === "rock"){
+   alert('you win')
+ } else if (userChoice === "scissors" && computerChoice === "paper"){
+   alert('you win')
+ } else{
+   alert('you lose')
+ }
 
 
 
@@ -13,14 +22,10 @@ console.log("The user chooses: " + userChoice);
   console.log("The computer chooses: " + computerChoice);
   // Your declareWinner function goes below
 function declareWinner(userChoice,computerChoice){
-  if ("userChoice is paper")
-   statement1
+  if (userChoice === "rock" && computerChoice === "scissors") {
+     alert('you win')
+   }
 }
-
-
-
-
-
 
 
 
